935afa2552309924fb6409185353dda2d0b6f67c
Sarajuddin <sarajuddinkazgar@gmail.com>
2023-02-13 21:14:02 +0530

    files added

---
+++ 14. Sphere_Volume.py
@@ -0,0 +1,6 @@
+# 14. Write a Python program to get the volume of a sphere with radius 6.
+
+
+radius = 6
+volume = 2/3*3.14159*radius**3
+print(f"Volume of sphere with radius {radius} is : {volume}")
